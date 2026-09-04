a415c84b253169c0947f1932163f5bc77920debc
Benson Lee <102480522+bensonnlee@users.noreply.github.com>
2026-09-04 08:48:48 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # key: value
+        # all letters sorted: the actual string
+        # sorted(string): [[cat, act]]
+
+        # create a list
+            # create a sublist of each of the values
+
+        seen = {}
+
+        for string in strs:
+            if str(sorted(string)) in seen:
+                seen[str(sorted(string))].append(string)
+            else:
+                seen[str(sorted(string))] = [string]
+
+        return list(seen.values())
+
