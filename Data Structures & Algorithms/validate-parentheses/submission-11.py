e8120e0637ea3ee93de4b1f8dbb21db326f54fc2
Benson Lee <102480522+bensonnlee@users.noreply.github.com>
2026-09-09 00:54:32 -0700

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        char_map = {
+            "{": "}",
+            "[": "]",
+            "(": ")",
+        }
+
+        # input = ()
+        # stack = 
+        # val = (
+
+        for char in s:
+            if char in char_map:
+                stack.append(char)
+            else:
+                if not stack:
+                    return False
+                val = stack.pop()
+                if char != char_map[val]:
+                    return False
+
+        if stack:
+            return False
+            
+        return True
