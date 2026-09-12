a25f89aee7ed6f858ea22d638223398e1a84777e
Benson Lee <102480522+bensonnlee@users.noreply.github.com>
2026-09-11 22:57:41 -0700

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+
+        fresh = set()
+        rotten = set()
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh.add((r, c))
+                if grid[r][c] == 2:
+                    rotten.add((r, c))
+
+        time = 0
+
+        while fresh:
+            to_add = set()
+            for r, c in rotten:
+                for dr, dc in ((r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)):
+                    if (dr, dc) in fresh:
+                        fresh.remove((dr, dc))
+                        to_add.add((dr, dc))
+            time += 1
+
+            if not to_add:
+                return -1
+            rotten = set()
+            for r, c in to_add:
+                rotten.add((r, c))
+
+        return time
+    
