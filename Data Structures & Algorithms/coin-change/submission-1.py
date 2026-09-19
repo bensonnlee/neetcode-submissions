17ce3164aa75ccbb793dc6dc2b9f2d3fc3994ddf
Benson Lee <102480522+bensonnlee@users.noreply.github.com>
2026-09-19 16:23:32 -0700

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        """
+        constraints:
+            coins must add up to the amount
+            if impossible, return -1
+            the amount of coins must be as minimal as possible
+
+        thoughts:
+            bottom-up approach
+        """
+        change = [float('inf')] * (amount + 1)
+        change[0] = 0
+
+        for i in range(1, len(change)):
+            for c in coins:
+                if i - c >= 0:
+                    change[i] = min(change[i], 1 + change[i - c])
+        
+        return -1 if change[-1] == float('inf') else change[-1]
